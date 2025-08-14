bc63fa0d163ae64c3ad580811c01a5fa410d13c1
Maryiah <marymansha15@gmail.com>
2025-08-14 18:05:57 +0100

    configure linting and prettier

--- eslint.config.js
+++ eslint.config.js
@@ -3,8 +3,10 @@ import storybook from "eslint-plugin-storybook";
 
 import js from '@eslint/js'
 import globals from 'globals'
+import react from 'eslint-plugin-react'
 import reactHooks from 'eslint-plugin-react-hooks'
 import reactRefresh from 'eslint-plugin-react-refresh'
+import prettier from 'eslint-plugin-prettier'
 import tseslint from 'typescript-eslint'
 import { globalIgnores } from 'eslint/config'
 
@@ -17,10 +19,23 @@ export default tseslint.config([
       tseslint.configs.recommended,
       reactHooks.configs['recommended-latest'],
       reactRefresh.configs.vite,
+      "plugin:prettier/recommended",
     ],
     languageOptions: {
       ecmaVersion: 2020,
       globals: globals.browser,
     },
+    plugins: {
+      react,
+      prettier,
+    },
+    rules: {
+      'prettier/prettier': 'error',
+    },
+    settings: {
+      react: {
+        version: 'detect'
+      }
+    }
   },
 ], storybook.configs["flat/recommended"]);
